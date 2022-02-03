d1b267978e9421d0ed97c4fa3dad91819e9593db
David Flay <flay@jlab.org>
2022-02-03 10:13:28 -0500

    Cleaned up the test examples script

--- beam/Test.cxx
+++ beam/Test.cxx
@@ -25,24 +25,75 @@
 #include "./src/bcmUtilities.cxx"
 #include "./src/ROOTFileManager.cxx"
 
+int testROOTFileMetaData();
+int testJSONManager(); 
+int testRDataFrame();
+int testROOTFileManager(); 
+
 int Test(){
 
-   std::string prefix = "/lustre19/expphy/volatile/halla/sbs/flay/GMnAnalysis/rootfiles";
+   int rc=0;
+   // rc = testROOTFileMetaData();
+   // rc = testJSONManager();
+   rc = testRDataFrame();
+   // rc = testROOTFileManager();
 
-   util::rootData_t rfData; 
+   return 0;
+}
+//______________________________________________________________________________
+int testROOTFileMetaData(){
+
+   JSONManager *jmgr = new JSONManager("./input/json/test.json");
+
+   std::string rfPath = jmgr->GetValueFromKey_str("ROOTfile-path");
+   int run=11913;
+   std::vector<int> data; 
+   int rc = util_df::GetROOTFileMetaData(rfPath.c_str(),run,data); 
+
+   int stream=-1,begSeg=-1,endSeg=-1; 
+   if(rc==0){
+      stream = data[0]; 
+      begSeg = data[1]; 
+      endSeg = data[2];
+      std::cout << Form("Run %d, begSeg = %d, endSeg = %d",run,begSeg,endSeg) << std::endl;
+   }
+
+   delete jmgr; 
+
+   return 0;
+}
+//______________________________________________________________________________
+int testJSONManager(){
+   JSONManager *jmgr = new JSONManager("./input/json/test.json");
+   jmgr->Print(); 
+
+   std::string cutFile = jmgr->GetValueFromKey_str("cut-list"); 
+   std::cout << cutFile << std::endl; 
+
+   std::vector<double> cc; 
+
+   int NCC = jmgr->GetValueFromKey<int>("ncc"); 
+   jmgr->GetVectorFromKey<double>("calib-coeffs",NCC,cc); 
+
+   for(int i=0;i<NCC;i++){
+      std::cout << cc[i] << std::endl;
+   }
+
+   delete jmgr;
+
+   return 0;
+}
+//______________________________________________________________________________
+int testRDataFrame(){
+
    // path to the ROOT file
-   rfData.fileName      = prefix + "/gmn_replayed-beam_13297_stream0_seg0_2.root";
-   // path to the file that defines the ROOTfile structure
-   // this is a csv of the form: treeName,branchName,bufferSize  
-   // example (must include the header below)
-   // treeName,branchName,bufSize 
-   // TSsbs,sbs.bcm.unser.rate,D (D = Double_t, see TTree class def for details) 
-   rfData.structurePath = "./input/format/beam.csv";   
+   std::string prefix   = "/lustre19/expphy/volatile/halla/sbs/flay/GMnAnalysis/rootfiles";
+   std::string fileName = prefix + "/gmn_replayed-beam_13297_stream0_seg0_2.root";
  
-   std::cout << "TRYING FILE: " << rfData.fileName << std::endl;
+   std::cout << "TRYING FILE: " << fileName << std::endl;
 
    // ROOT dataframe
-   std::string_view rfName{rfData.fileName}; 
+   std::string_view rfName{fileName}; 
    ROOT::RDataFrame sbsDF("TSsbs",rfName,{"TSsbs"});
 
    // make a cut (lambda function) -- this doesn't work with tree variables with names like "branch.leaf.x"
@@ -56,86 +107,42 @@ int Test(){
    c1->cd();
    h->DrawClone();
    c1->Update(); 
+
+   return 0;
+}
+//______________________________________________________________________________
+int testROOTFileManager(){
+
+   std::string prefix   = "/lustre19/expphy/volatile/halla/sbs/flay/GMnAnalysis/rootfiles";
+   std::string fileName = prefix + "/gmn_replayed-beam_13297_stream0_seg0_2.root";
+   // path to the file that defines the ROOTfile structure
+   // this is a csv of the form: treeName,branchName,bufferSize  
+   // example (must include the header below)
+   // treeName,branchName,bufSize 
+   // TSsbs,sbs.bcm.unser.rate,D (D = Double_t, see TTree class def for details) 
+   std::string structurePath = "./input/format/beam.csv";   
+
+   util::ROOTFileManager *rfMgr = new util::ROOTFileManager();
+   rfMgr->LoadFile(fileName.c_str(),structurePath.c_str());
+   rfMgr->Print(); 
  
-   // util::ROOTFileManager *rfMgr = new util::ROOTFileManager();
-   // rfMgr->LoadFile(rfData.fileName.Data(),rfData.structurePath.Data());
-   // rfMgr->Print(); 
+   // make a histogram  
+   TH1F *h = rfMgr->GetTH1F("TSsbs","sbs.bcm.unser.rate",1000,0,900E+3);
+
+   TCanvas *c1 = new TCanvas("c1","Test Plot",1200,800);
+   c1->cd();
+
+   h->Draw();
+   c1->Update(); 
+
+   // get a vector of data  
+   std::vector<double> unserRate; 
+   rfMgr->GetVector<double>("TSsbs","sbs.bcm.unser.rate",unserRate); 
+
+   const int N = unserRate.size();
+   for(int i=0;i<N;i++) std::cout << Form("%.3lf",unserRate[i]) << std::endl;
  
-   // // make a histogram  
-   // TH1F *h = rfMgr->GetTH1F("TSsbs","sbs.bcm.unser.rate",1000,0,900E+3);
-
-   // TCanvas *c1 = new TCanvas("c1","Test Plot",1200,800);
-   // c1->cd();
-
-   // h->Draw();
-   // c1->Update(); 
-
-   // // get a vector of data  
-   // std::vector<double> unserRate; 
-   // rfMgr->GetVector<double>("TSsbs","sbs.bcm.unser.rate",unserRate); 
-
-   // const int N = unserRate.size();
-   // for(int i=0;i<N;i++) std::cout << Form("%.3lf",unserRate[i]) << std::endl;
- 
-   // delete rfMgr; 
-
-   // scalerData_t data; 
-   // data.unserCurrent = 15.; 
-   // std::cout << data.Print("rate") << std::endl;
-
-   // JSONManager *jmgr = new JSONManager("./input/json/test.json");
-   // jmgr->Print(); 
-
-   // std::string cutFile = jmgr->GetValueFromKey_str("cut-list"); 
-   // std::cout << cutFile << std::endl; 
-
-   // std::vector<double> cc; 
-
-   // int NCC = jmgr->GetValueFromKey<int>("ncc"); 
-   // jmgr->GetVectorFromKey<double>("calib-coeffs",NCC,cc); 
-
-   // for(int i=0;i<NCC;i++){
-   //    std::cout << cc[i] << std::endl;
-   // }
-
-   // std::string rfPath = jmgr->GetValueFromKey_str("ROOTfile-path");
-   // int run=11913;
-   // std::vector<int> data; 
-   // int rc = util_df::GetROOTFileMetaData(rfPath.c_str(),run,data); 
-
-   // int stream=-1,begSeg=-1,endSeg=-1; 
-   // if(rc==0){
-   //    stream = data[0]; 
-   //    begSeg = data[1]; 
-   //    endSeg = data[2];
-   //    std::cout << Form("Run %d, begSeg = %d, endSeg = %d",run,begSeg,endSeg) << std::endl;
-   // }
-
-   // // settings 
-   // bool logScale   = false;
-
-   // gStyle->SetOptStat(0);
-
-   // int rc=0;
-
-   // TString prefix;
-   // std::vector<codaRun_t> runList;  
-   // rc = bcm_util::LoadRuns(runPath,prefix,runList);
-   // if(rc!=0) return 1; 
-
-   // BCMManager *mgr = new BCMManager();
-   // mgr->SetDebug();
-   // mgr->EnableEPICS();
-
-   // TString filePath;  
-   // const int NR = runList.size();  
-   // for(int i=0;i<NR;i++){ 
-   //    filePath = Form("%s/gmn_replayed-beam_%d_stream%d_seg%d_%d.root",
-   //                    prefix.Data(),runList[i].runNumber,runList[i].stream,runList[i].segmentBegin,runList[i].segmentEnd);
-   //    mgr->LoadFile(filePath,runList[i].runNumber);
-   // }
-
-   // mgr->Print("Left"); 
+   delete rfMgr; 
 
    return 0;
 }
