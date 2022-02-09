c2a6bcfb86829fa804c936dfc33d5bf4011b9ea7
David Flay <flay@jlab.org>
2022-02-09 14:33:59 -0500

    Added TODO notes

--- beam/bcmCalib.py
+++ beam/bcmCalib.py
@@ -4,19 +4,19 @@
 import os 
 import sys
 
-# read in a config file!
+# TODO: 
+# 1. read in a config file
+# 2. make config file to each script below come from a JSON file (step 1) 
 
-varList = ["unser","u1","unew","d1","d3","d10","dnew"]
 
 #===============================================================================
-# process all cuts 
-confFile = "./input/json/bcm-calib_02-05-22.json" # make this an input argument!
-
+# process all cuts (on all BCM variables)  
+varList    = ["unser","u1","unew","d1","d3","d10","dnew"]
+confFile   = "./input/json/bcm-calib_02-05-22.json" # make this an input argument!
 scriptName = "bcmCalibProcessCuts.cxx"
 for var in varList: 
   cmd = "analyzer -q -b -l '{0}(\"{1}\",\"{2}\")'".format(scriptName,confFile,var)
   print(cmd)
   os.system(cmd) 
-
 #===============================================================================
 # compute BCM calibration coefficients 
