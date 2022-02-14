e585efb23e3135b2d3e6970de6618ae71ecc88a8
David Flay <flay@jlab.org>
2022-02-14 13:45:27 -0500

    Added a utility namespace for python scripts

---
+++ beam/util.py
@@ -0,0 +1,18 @@
+# helpful utility functions
+import os 
+import sys
+import json
+import csv
+import shutil
+import datetime
+
+#_______________________________________________________________________________
+def utcNow(): 
+   theTime = datetime.datetime.utcnow() 
+   return int(theTime.strftime("%s")) 
+#_______________________________________________________________________________
+def deleteDir(path): 
+   shutil.rmtree(path) 
+#_______________________________________________________________________________
+def createDir(path): 
+   os.makedirs(path)
