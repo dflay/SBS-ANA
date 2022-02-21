d33bd030304fbc1418590a143d151a54529d20ba
David Flay <flay@jlab.org>
2022-02-21 15:58:10 -0500

    Minor cleanup

--- beam/bcmCheckCharge.cxx
+++ beam/bcmCheckCharge.cxx
@@ -32,9 +32,8 @@ int bcmCheckCharge(const char *confPath){
 
    // read input configuration file 
    JSONManager *jmgr = new JSONManager(confPath);
-   std::string prefix       = jmgr->GetValueFromKey_str("ROOTfile-path"); 
-   std::string runPath      = jmgr->GetValueFromKey_str("run-path");  
-   std::string cutPath      = jmgr->GetValueFromKey_str("cut-path"); 
+   std::string prefix  = jmgr->GetValueFromKey_str("ROOTfile-path"); 
+   std::string runPath = jmgr->GetValueFromKey_str("run-path");  
    
    BCMManager *mgr = new BCMManager("NONE","NONE",false);
 
@@ -45,31 +44,12 @@ int bcmCheckCharge(const char *confPath){
    util_df::LoadBCMData(runList,mgr); 
    if(rc!=0) return 1; 
 
-   // do stats by run number 
-   std::vector<scalerData_t> sbs,lhrs;
-   mgr->GetVector_scaler("sbs" ,sbs);  
-   mgr->GetVector_scaler("Left",lhrs);  
-
-   std::vector<epicsData_t> edata; 
-   mgr->GetVector_epics(edata);  
-
-   // // load cuts 
-   // std::vector<cut_t> cutList; 
-   // cut_util::LoadCuts(cutPath.c_str(),cutList);
-   // // apply cuts  
-   // cut_util::ApplyCuts(cutList,rawData,data);
-
-   // sort the data by run number 
-   std::sort(sbs.begin() ,sbs.end() ,compareScalerData_byRun); 
-   std::sort(lhrs.begin(),lhrs.end(),compareScalerData_byRun); 
-   std::sort(edata.begin(),edata.end(),compareEPICSData_byRun); 
-
    std::vector<int> rr; 
    mgr->GetRunList(rr); 
    const int NNR = rr.size();
 
    const int N = 6; 
-   TString varC[N] = {"u1.current","unew.current","d1.current","d3.current","d10.current","dnew.current"};
+   TString var[N] = {"u1.current","unew.current","d1.current","d3.current","d10.current","dnew.current"};
 
    charge_t qData; 
    std::vector<scalerData_t> runData;
@@ -81,9 +61,9 @@ int bcmCheckCharge(const char *confPath){
       std::cout << Form("Run %d: ",rr[i]) << std::endl;
       // loop over all BCMs and compute charge
       for(int j=0;j<N;j++){ 
-	 bcm_util::GetCharge(varC[j].Data(),runData,qData);
-         std::cout << Form("   %s: totalTime = %.3lf sec, Q = (%.3E ± %.3E) C",
-                           varC[j].Data(),qData.totalTime,qData.value,qData.error) << std::endl;
+	 bcm_util::GetCharge(var[j].Data(),runData,qData);
+         std::cout << Form("   %s: totalTime = %.3lf sec (%.1lf min), Q = (%.3E ± %.3E) C",
+                           var[j].Data(),qData.totalTime,qData.totalTime/60.,qData.value,qData.error) << std::endl;
       }
       // set up for next run
       runData.clear();
