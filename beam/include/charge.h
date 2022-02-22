e7211c550f2a199c0dfc382bf57736637287c7e1
David Flay <flay@jlab.org>
2022-02-22 16:49:32 -0500

    Added a script to compare BCM calibration result data sets

--- beam/include/charge.h
+++ beam/include/charge.h
@@ -7,18 +7,19 @@
 #include <iostream> 
 
 typedef struct charge { 
+   std::string info;   // general run information
    double totalTime;   // total time of run (sec)  
    double value;       // charge in Coulombs 
    double error;       // uncertainty in Coulombs
    int runNumber;      // run number 
    // constructor 
    charge(): 
-      totalTime(0),value(0),error(0),runNumber(0)
+      info("NONE"),totalTime(0),value(0),error(0),runNumber(0)
    {} 
    // useful functions 
    void Print(){
       std::cout << "-------------------------" << std::endl;
-      std::cout << "run:        " << runNumber << std::endl; 
+      std::cout << "Run number: " << runNumber << std::endl; 
       std::cout << "Total time: " << Form("%.3lf sec",totalTime)     << std::endl;
       std::cout << "Charge:     " << Form("%.3E ± %.3E",value,error) << std::endl;
       std::cout << "-------------------------" << std::endl;
